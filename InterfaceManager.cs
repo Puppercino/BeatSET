a8ddac1958e374b27712acd650c76fbac00849b7
Puppercino <jordyalexlyall@gmail.com>
2020-07-22 16:59:54 +1000

    Initial Upload of CSharp Files.

---
+++ InterfaceManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InterfaceManager : MonoBehaviour
+{
+    [Header("Base Interface Settings")]
+    public CanvasGroup targetCanvasGroup;
+
+    [Header("Dialog Box Settings")]
+    public CanvasGroup dialogBox;
+    public Text dialogBoxTitle;
+    public Text dialogBoxMessage;
+    public bool dialogConfirmed;
+
+    private void Start()
+    {
+        targetCanvasGroup.alpha = 1;
+
+        dialogBox.alpha = 0;
+        dialogBox.interactable = false;
+        dialogBox.blocksRaycasts = false;
+
+    }
+
+    public void ShowDialog()
+    {
+        StartCoroutine(DialogManager());
+        Debug.Log("Coroutine for DialogBox started.");
+    }
+
+    public void ConfirmDialog() // Apply this function to the OK Button.
+    {
+        dialogConfirmed = true;
+    }
+
+    private IEnumerator DialogManager()
+    {
+        dialogBox.alpha = 1;
+        dialogBoxTitle.text = "3";
+        dialogBoxMessage.text = "e";
+        dialogBox.interactable = true;
+        dialogBox.blocksRaycasts = true;
+        
+        yield return WaitForConfirmation();
+        
+        dialogBox.alpha = 0;
+        dialogBox.interactable = false;
+        dialogBox.blocksRaycasts = false;
+
+        IEnumerator WaitForConfirmation()
+        {
+            dialogConfirmed = false;
+            while (!dialogConfirmed)
+            {
+                yield return null;
+            }
+            
+        }
+    }
+}
---
+++ SaveManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+
+ 
+    [Header("Save Information")] 
+    public bool saveExists;
+    public int saveFileVersion;
+    public string playerName;
+
+    private void Start()
+    {
+        // If no save file exists, create save and return.
+        if (!saveExists) return;
+        saveExists = PlayerPrefs.HasKey("SaveFileVersion");
+        saveFileVersion = PlayerPrefs.GetInt("SaveFileVersion");
+        playerName = PlayerPrefs.GetString("PlayerName");
+    }
+
+    private void CreateSave()
+    {
+        saveFileVersion = 0;
+        playerName = "Tester";
+        PlayerPrefs.SetInt("SaveFileVersion", saveFileVersion);
+        PlayerPrefs.SetString("PlayerName", playerName);
+        PlayerPrefs.Save();
+    }
+    
+    // Shortcut to simply saving the PlayerPrefs.
+    private void Save() {PlayerPrefs.Save();}
+    
+    private void ResetSave()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    private void Load()
+    {
+        if (!saveExists)
+        {
+            Debug.Log("No save file exists.");
+            return;
+        }
+        PlayerPrefs.GetInt("SaveFileVersion", saveFileVersion);
+        PlayerPrefs.GetString("PlayerName", playerName);
+    }
+}
